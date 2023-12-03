aef4e7fd3fb1e68ee90b396d43fe27a522a58c12
Rohith172Reddy <rohithg7410@gmail.com>
2023-12-03 15:11:27 -0600

    My_Portfolio

---
+++ Portfolio/my_portfolio/src/components/Projects.jsx
@@ -0,0 +1,67 @@
+import React,{useEffect} from 'react'
+import Hostel from '../assests/projects/Hostel.png'
+import Weather from '../assests/projects/Weather.png'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+const Projects = () => {
+  useEffect(()=> {
+    AOS.init({duration: 1000})
+})
+
+  const Projects = () => [
+    
+    {
+      id: 1,
+      src: Hostel,
+      desc: "This android app is made using Flutter, Firebase which is a Hostel Management System for college...",
+      demo: "https://github.com/dipayansarkar47/hostel_management",
+      code: "https://github.com/dipayansarkar47/hostel_management"
+    },
+    
+    {
+      id: 2,
+      src: Weather,
+      // desc: "It is a Weather application which shows temperatures of various cities across the globe made using REST API...",
+      demo: "https://dipayansarkar47.github.io/WeatherApp/",
+      code: "https://github.com/dipayansarkar47/WeatherApp"
+    },
+    
+  ]
+
+
+  return (
+    <div name="projects" className='h-auto max-h-screen-lg  bg-gradient-to-b from-black to-gray-800 w-full text-white
+     md:h-screen'>
+      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <h2 className='text-4xl font-bold inline border-b-4 border-gray-400'>Projects</h2>
+          <p className='py-6'>Check out some of my work </p>
+        </div>
+        <div   className='grid sm:grid-cols-2 md:grid-cols-3 mb-20 gap-8 px-12 sm:px-0'>
+          {
+            Projects().map(({ id, src, desc, demo, code }) => (
+
+              <div data-aos="fade-in" data-aos-duration="500" key={id} className='shadow-md shadow-gray-600 rounded-lg'>
+                <img src={src} alt="Ai" className='rounded-md duration-200 hover:scale-105' />
+                <p className='p-2 text-justify font-extralight'>{desc}</p>
+                <div className='flex items-center justify-center'>
+                  
+                  <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 bg-gradient-to-l from-gray-700 to-blue-900 rounded-md'>
+                    <a href={demo}>Demo</a>
+                  </button>
+                  <button  className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 bg-gradient-to-l from-purple-900 to-gray-700 rounded-md'>
+                    <a href={code}>Code</a>
+                  </button>
+                </div>
+              </div>
+
+            ))
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Projects
